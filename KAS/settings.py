f7a53c0b88131323231eb687326332477cb8ac92
kevalpaghadal <myleptop.vue0m8eg@gmail.com>
2024-08-04 11:26:18 +0530

    update some files

--- KAS/settings.py
+++ KAS/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'category',
     'account',
     'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'cart.context_processors.counter',
             ],
         },
     },
